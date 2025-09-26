428987af97a5cad16d4111f929b120c94fcc833b
Victoria Cepeda <142287614+VictoriaCepedaMarquinez@users.noreply.github.com>
2025-09-26 19:08:12 -0300

    implementacion entity Estudiante y EstudianteRepository

--- src/repositories/EstudianteRepository.java
+++ src/repositories/EstudianteRepository.java
@@ -1,4 +1,9 @@
 package repositories;
 
+import entities.Estudiante;
+
 public interface EstudianteRepository {
+    Estudiante getEstudiante(int dni);
+    void saveEstudiante(Estudiante estudiante);
+    void deleteEstudiante(int dni);
 }
--- src/repositories/EstudianteRepositoryImpl.java
+++ src/repositories/EstudianteRepositoryImpl.java
@@ -1,4 +1,38 @@
 package repositories;
 
+import entities.Estudiante;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
 public class EstudianteRepositoryImpl implements EstudianteRepository{
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public EstudianteRepositoryImpl(EntityManager em) {
+    }
+
+    public Estudiante getEstudiante(int dni) {
+        return em.find(Estudiante.class, dni);
+    }
+
+    @Override
+    @Transactional
+    public void saveEstudiante(Estudiante estudiante) {
+        if (estudiante.getNombre() == null){
+            em.persist(estudiante);
+        }else {
+            em.merge(estudiante);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void deleteEstudiante(int dni) {
+        Estudiante estudiante = em.find(Estudiante.class, dni);
+        if (estudiante != null) {
+            em.remove(estudiante);
+        }
+    }
 }
